06fed08b7b4f20108e0df51790e660b17583c19e
KhaVNguyen <kha.vng@gmail.com>
2019-11-15 18:11:56 -0800

    Redirecting to login when not logged in

---
+++ spotgen/src/Components/ExplorePage/ExplorePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Navbar extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (<div>Explore</div>);
+    }
+}
