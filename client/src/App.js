95f2895f53857b2e09dc9d12e92fac87384308ce
KhaVNguyen <kha.vng@gmail.com>
2019-11-15 17:31:25 -0800

    Some login routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,42 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+import Login from "./Components/Login/Login";
 import Navbar from "./Components/Navbar/Navbar";
+import ExplorePage from "./Components/ExplorePage/ExplorePage"
 
 export default class App extends Component {
-  constructor(){
+  constructor() {
     super();
     const params = this.getHashParams();
-    console.log(params);
+    const token = params.access_token;
+    this.state = {
+      loggedIn: token ? true : false,
+    }
   }
 
   getHashParams() {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
+      q = window.location.hash.substring(1);
     e = r.exec(q)
     while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
     }
     return hashParams;
   }
-  
+
   render() {
     return (
       <div className="App">
         {/* <a href='http://localhost:8888'>Login to Spotify</a> */}
-        <Navbar />
+        {/* <Login /> */}
+        <Router>
+          <Route exact path='/' component={ExplorePage} />
+          <Route exact path='/login' component={Login} />
+        </Router>
       </div>
     );
   }
