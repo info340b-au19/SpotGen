f86efd801f6a399515717590f2865a306e9dda17
KhaVNguyen <kha.vng@gmail.com>
2019-11-17 16:08:32 -0800

    add username to navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,26 +1,36 @@
-import React, { Component } from 'react';
-import { NavLink as Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import { NavLink as Link } from "react-router-dom";
 
 export default class Navbar extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <div id="top-bar">
-                <div id="logo-wrapper">
-                    <img src={process.env.PUBLIC_URL + "images/spotgen-logo.png"} alt="spotgen-logo" />
-                    <Link to='/explore' className="tab" activeClassName="selected">Explore</Link>
-                    <Link to='/create' className="tab" activeClassName="selected">Create</Link>
-
-
-                </div>
-                <div id="profile-wrapper">
-                    <span className="username"></span>
-                    <img src={process.env.PUBLIC_URL + "images/profile.svg"} alt="profile-icon" />
-                </div>
-            </div>);
-    }
+  render() {
+    return (
+      <div id="top-bar">
+        <div id="logo-wrapper">
+          <img
+            src={process.env.PUBLIC_URL + "images/spotgen-logo.png"}
+            alt="spotgen-logo"
+          />
+          <Link to="/explore" className="tab" activeClassName="selected">
+            Explore
+          </Link>
+          <Link to="/create" className="tab" activeClassName="selected">
+            Create
+          </Link>
+        </div>
+        <div id="profile-wrapper">
+          <span className="username">
+            {this.props.userData ? this.props.userData.display_name : "User"}
+          </span>
+          <img
+            src={process.env.PUBLIC_URL + "images/profile.svg"}
+            alt="profile-icon"
+          />
+        </div>
+      </div>
+    );
+  }
 }
