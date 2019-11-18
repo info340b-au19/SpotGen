ee4b24a0417c57aaa1fac23b519bc8787da23359
KhaVNguyen <kha.vng@gmail.com>
2019-11-17 16:37:17 -0800

    add username to the explore page

--- src/Components/ExplorePage/ExplorePage.js
+++ src/Components/ExplorePage/ExplorePage.js
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 import Navbar from "../Navbar/Navbar";
 
+import { getUserData } from "../../Helper";
+
 export default class ExplorePage extends Component {
   constructor() {
     super();
+    this.state = {
+      userData: {}
+    };
+  }
+
+  async componentDidMount() {
+    let userData = await getUserData(this.props.accessToken);
+    this.setState({
+      userData: userData
+    });
   }
 
   render() {
     return (
       <div className="page">
-        <Navbar userData={this.props.userData} />
+        <Navbar userData={this.state.userData} />
         <h1>Explore</h1>
         <span></span>
       </div>
