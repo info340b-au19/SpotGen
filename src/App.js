41ee0857e0101caffeb30ac5bb8cc257fbca2855
KhaVNguyen <kha.vng@gmail.com>
2019-11-17 16:34:25 -0800

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,45 +2,19 @@ import React, { Component } from "react";
 import { Route, Redirect } from "react-router-dom";
 
 import Login from "./Components/Login/Login";
-import Navbar from "./Components/Navbar/Navbar";
 import ExplorePage from "./Components/ExplorePage/ExplorePage";
 import CreatePage from "./Components/CreatePage/CreatePage";
+
+import { getAccessToken } from "./Helper";
 
 export default class App extends Component {
   constructor() {
     super();
-    const token = this.getAccessToken();
+    const token = getAccessToken();
     this.state = {
       loggedIn: token ? true : false,
       accessToken: token
     };
-  }
-
-  async componentDidMount() {
-    let userData = await this.getUserData(this.state.accessToken);
-    this.setState({ userData: userData });
-  }
-
-  getAccessToken() {
-    let accessToken = document.cookie.match(
-      "(^|[^;]+)\\s*" + "accessToken" + "\\s*=\\s*([^;]+)"
-    );
-    return accessToken ? accessToken.pop() : "";
-  }
-
-  async getUserData(accessToken) {
-    let url = "https://api.spotify.com/v1/me";
-    let data = await fetch(url, {
-      method: "GET",
-      headers: {
-        Authorization: "Bearer " + accessToken
-      }
-    });
-    let userData = await data.json();
-    return userData;
-    // let userID = userData.id;
-    // state["userID"] = userID;
-    // state["userPlaylists"] = await getUserPlaylists(userID, accessToken);
   }
 
   render() {
@@ -55,7 +29,6 @@ export default class App extends Component {
             <ExplorePage
               {...routerProps}
               accessToken={this.state.accessToken}
-              userData={this.state.userData}
             />
           )}
         />
@@ -66,7 +39,6 @@ export default class App extends Component {
             <ExplorePage
               {...routerProps}
               accessToken={this.state.accessToken}
-              userData={this.state.userData}
             />
           )}
         />
@@ -74,11 +46,7 @@ export default class App extends Component {
           exact
           path="/create"
           render={routerProps => (
-            <CreatePage
-              {...routerProps}
-              accessToken={this.state.accessToken}
-              userData={this.state.userData}
-            />
+            <CreatePage {...routerProps} accessToken={this.state.accessToken} />
           )}
         />
       </div>
--- src/Components/CreatePage/SelectPlaylists.js
+++ src/Components/CreatePage/SelectPlaylists.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class SelectPlaylists extends Component {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    render() {
-        return (
-            <div id="select-playlists">
-                <div class="step">
-                    <div class="step-number">
-                        1
-                    </div>
-                    <div>
-                        <h2>Select Playlists</h2>
-                    </div>
-                </div>
-                <p class="section-description">Select playlists to grab songs from for your new playlist</p>
-                <div id="playlists-wrapper">
-                </div>
-            </div>);
-    }
+  render() {
+    return (
+      <div id="select-playlists">
+        <div className="step">
+          <div className="step-number">1</div>
+          <div>
+            <h2>Select Playlists</h2>
+          </div>
+        </div>
+        <p className="section-description">
+          Select playlists to grab songs from for your new playlist
+        </p>
+        <div id="playlists-wrapper"></div>
+      </div>
+    );
+  }
 }
