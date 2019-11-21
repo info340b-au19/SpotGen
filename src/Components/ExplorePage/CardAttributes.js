a4745ac70ccb6f4f4f63cf689ff57a61c4ff058a
KhaVNguyen <kha.vng@gmail.com>
2019-11-21 04:42:06 -0800

    Try catches

---
+++ src/Components/ExplorePage/CardAttributes.js
@@ -0,0 +1,66 @@
+/* eslint-disable no-multi-str */
+export function cardAttributes() {
+  let genreObjects = [
+    {
+      genreAPI: "alt-rock",
+      genreName: "Alternative Rock",
+      description:
+        "A style of rock music that emerged from the independent music underground\
+            of the 1970s and became widely popular in the 1980s."
+    },
+    {
+      genreAPI: "hip-hop",
+      genreName: "Rap",
+      description:
+        "A style of popular music in which an insistent, recurring beat pattern provides\
+            the background and counterpoint for rapid, slangy, and often boastful\
+            rhyming patter glibly intoned by a vocalist or vocalists."
+    },
+    {
+      genreAPI: "country",
+      genreName: "Country",
+      description:
+        "A genre of popular music that originated in the southern United States in the early 1920s.\
+            It takes its roots from genres such as American folk music and blues."
+    },
+    {
+      genreAPI: "folk",
+      genreName: "Folk",
+      description:
+        "Includes traditional folk music and the genre that evolved from it during the 20th-century\
+          folk revival. Some types of folk music may be called world music."
+    },
+    {
+      genreAPI: "hard-rock",
+      genreName: "Hard-Rock",
+      description:
+        "A loosely defined subgenre of rock music that began in the mid-1960s, with the garage,\
+            psychedelic and blues rock movements. It is typified by a heavy use of aggressive vocals,\
+            distorted electric guitars, bass guitar, drums, and often accompanied with keyboards."
+    },
+    {
+      genreAPI: "classical",
+      genreName: "Classical",
+      description:
+        "Art music produced or rooted in the traditions of Western culture, including both liturgical\
+            (religious) and secular music."
+    },
+    {
+      genreAPI: "jazz",
+      genreName: "Jazz",
+      description:
+        "A music genre that originated in the African-American communities of New Orleans, United\
+            States. It originated in the late 19th and early 20th centuries, and developed from roots in blues\
+            and ragtime. Jazz is seen by many as 'America's classical music.'"
+    },
+    {
+      genreAPI: "holidays",
+      genreName: "Holidays",
+      description:
+        "Primarily confined to the Christmas season, at which time classics like 'White Christmas'\
+        make their annual return to radio playlists and choir songbooks; in the strictest sense,\
+        however, its music is typically heard in celebration of a given holiday event."
+    }
+  ];
+  return genreObjects;
+}
